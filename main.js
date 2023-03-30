5452674ebef3aa7e58252bb2b9038e356c5a3b52
Jahanvi <128139334+G-JAHNAVI@users.noreply.github.com>
2023-03-30 10:39:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const resultBox = document.querySelector("input[type='text']"); 
+const buttons = document.querySelectorAll("input[type='button']");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.value === "clear") {
+      resultBox.value = "";
+    } else if (e.target.value === "=") {
+      if (resultBox.value) resultBox.value = eval(resultBox.value); 
+    } else if (e.target.value === "del") {
+      if (resultBox.value) resultBox.value = resultBox.value.slice(0, -1);
+    } else {
+      resultBox.value += e.target.value;
+    }
+  });
+});
